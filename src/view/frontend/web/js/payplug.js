39ca2ae72814e8bf52677697fbd530d66b6a5a2f
Manuel Mesquita <mmesquita-ext@payplug.com>
2024-05-10 11:32:24 +0100

    MAG-262 fix for applepay show on chrome

--- src/view/frontend/web/js/payplug.js
+++ src/view/frontend/web/js/payplug.js
@@ -9,6 +9,17 @@
 
     }
   });
+
+  window.addEventListener('checkout:payment:method-activate', (event) => {
+    if (typeof window.ApplePaySession != "undefined" && window.ApplePaySession && ApplePaySession.canMakePayments()){
+    }else{
+
+      if (typeof(document.querySelector('#payment-method-option-payplug_payments_apple_pay')) != 'undefined' && document.querySelector('#payment-method-option-payplug_payments_apple_pay') != null){
+        document.querySelector('#payment-method-option-payplug_payments_apple_pay').remove();
+      }
+
+    }
+  });
 })()
 
 
