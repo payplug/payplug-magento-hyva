0ec7d96a5a197831fa5d70ff009ce50aa82454e7
sheyne <sheyne@dnd.fr>
2024-09-20 14:10:23 +0200

    release/v1.0.3: Update composer.json version to 1.0.3 and fix the hyva version in the api call
    Changelog: fixed

--- src/Plugin/TrackHyvaVersion.php
+++ src/Plugin/TrackHyvaVersion.php
@@ -5,32 +5,23 @@ declare(strict_types=1);
 namespace Hyva\CheckoutPayplug\Plugin;
 
 use Hyva\CheckoutPayplug\Provider\Config as PayplugHyvaConfig;
-use Payplug\Core\HttpClient;
-use Payplug\Payments\Helper\Config as PayplugMagentoConfig;
-use Payplug\Payments\Helper\Http\AbstractClient;
+use Payplug\Payments\Helper\Config;
 use Psr\Log\LoggerInterface;
 
 class TrackHyvaVersion
 {
     public function __construct(
-        protected PayplugMagentoConfig $payplugMagentoConfig,
         protected PayplugHyvaConfig $payplugHyvaConfig,
         protected LoggerInterface $logger
     ) {
     }
 
-    public function afterPlaceRequest(AbstractClient $subject, array $result): array
+    public function afterGetMagentoVersion(Config $subject, string $magentoVersion): string
     {
-        HttpClient::addDefaultUserAgentProduct(
-            'PayPlug-Magento2',
-            $this->payplugMagentoConfig->getModuleVersion(),
-            sprintf('Magento %s) %s (%s',
-                $this->payplugMagentoConfig->getMagentoVersion(),
-                $this->payplugHyvaConfig->getHyvaModuleName(),
-                $this->payplugHyvaConfig->getHyvaModuleVersion()
-            )
+        return sprintf('%s) %s (%s',
+            $magentoVersion,
+            $this->payplugHyvaConfig->getHyvaModuleName(),
+            $this->payplugHyvaConfig->getHyvaModuleVersion()
         );
-
-        return $result;
     }
 }
