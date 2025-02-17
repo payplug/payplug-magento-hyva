60ae0b5bb342db24205c650b768583cbe7587210
sheyne <sheyne@dnd.fr>
2025-02-17 11:34:24 +0100

    MAG-418: Dynamically update the svg logos and alt for the payments methods if the local is Italian.
    Changelog: Added

--- src/Block/Standard.php
+++ src/Block/Standard.php
@@ -1,86 +1,78 @@
 <?php
+
 namespace Hyva\CheckoutPayplug\Block;
 
 use Magento\Customer\Model\Session;
+use Magento\Framework\Locale\ResolverInterface;
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 use Payplug\Payments\Helper\Card;
 use Payplug\Payments\Model\Payment\Standard\ConfigProvider as Config;
 
 class Standard extends Template
 {
-  /**
-   * @var Card
-   */
-  private $helper;
+    public function __construct(
+        private Context $context,
+        private Session $customerSession,
+        private Card $helper,
+        private Config $config,
+        private ResolverInterface $localeResolver,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
 
-  /**
-   * @var Session
-   */
-  private $customerSession;
+    public function getConfig(): array
+    {
+        return $this->config->getConfig();
+    }
 
-  private $config;
+    /**
+     * Override the Hyva payment cards logo if we are on the IT local
+     */
+    protected function _prepareLayout(): self
+    {
+        parent::_prepareLayout();
 
-  /**
-   * @param Template\Context $context
-   * @param Session          $customerSession
-   * @param Card             $helper
-   * @param array            $data
-   */
-  public function __construct(
-    Template\Context $context,
-    Session $customerSession,
-    Card $helper,
-    Config $config,
-    array $data = [])
-  {
-    parent::__construct($context, $data);
+        $metadata = $this->getData('metadata');
 
-    $this->config = $config;
-    $this->customerSession = $customerSession;
-    $this->helper = $helper;
-  }
+        if ($this->localeResolver->getLocale() === 'it_IT') {
+            $metadata['icon']['src'] = 'Payplug_Payments::images/standard/payment-cards-it.svg';
+            $metadata['icon']['attributes']['alt'] = 'Visa Mastercard Postepay';
+        }
 
-  public function getConfig(): array
-  {
-    return $this->config->getConfig();
-  }
+        $this->setData('metadata', $metadata);
 
-  /**
-   * Get customer saved PayPlug cards
-   *
-   * @return \Payplug\Payments\Model\Customer\Card[]
-   */
-  public function getPayplugCards()
-  {
-    return $this->helper->getCardsByCustomer($this->customerSession->getCustomer()->getId(), true);
-  }
+        return $this;
+    }
 
-  /**
-   * Format card expiration date
-   *
-   * @param string $date
-   *
-   * @return string
-   */
-  public function getFormattedExpDate($date)
-  {
-    return $this->helper->getFormattedExpDate($date);
-  }
+    /**
+     * Get customer saved PayPlug cards
+     *
+     * @return \Payplug\Payments\Model\Customer\Card[]
+     */
+    public function getPayplugCards(): array
+    {
+        return $this->helper->getCardsByCustomer($this->customerSession->getCustomer()->getId(), true);
+    }
 
-  /**
-   * Build delete card url
-   *
-   * @param int $customerCardId
-   *
-   * @return string
-   */
-  public function getDeleteCardUrl($customerCardId)
-  {
-    return $this->_urlBuilder->getUrl('payplug_payments/customer/cardDelete', [
-      'customer_card_id' => $customerCardId
-    ]);
-  }
+    /**
+     * Format card expiration date
+     */
+    public function getFormattedExpDate(string $date): string
+    {
+        return $this->helper->getFormattedExpDate($date);
+    }
 
+    /**
+     * Build delete card url
+     */
+    public function getDeleteCardUrl(int $customerCardId): string
+    {
+        return $this->_urlBuilder->getUrl('payplug_payments/customer/cardDelete', [
+            'customer_card_id' => $customerCardId
+        ]);
+    }
 
 
 }
