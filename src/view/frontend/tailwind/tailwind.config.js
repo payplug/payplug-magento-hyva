38de419b1cf937795f635de1fc648bcf0b4f29a2
Benoit Alix <benoit.alix@dnd.fr>
2026-02-09 19:53:01 +0100

    MAG-642: Update CSS to support Tailwind V4
    MAG-642: Update CSS to support Tailwind V4

--- src/view/frontend/tailwind/tailwind.config.js
+++ src/view/frontend/tailwind/tailwind.config.js
@@ -1,30 +1,6 @@
-const oneyColor = '#81BC00';
-
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        oney: {
-          'DEFAULT': oneyColor,
-          lighter: '#ECF5D9'
-        }
-      },
-      borderColor: {
-        oney: {
-          'DEFAULT': oneyColor,
-          lighter: '#DCE0E8'
-        }
-      },
-      width: {
-        'payment-card': '33px',
-      },
-      height: {
-        'payment-card': '22px',
-      }
-    }
-  },
-  content: [
-      '../layout/**/*.xml',
-      '../templates/**/*.phtml'
-  ]
-}
+    content: [
+        '../templates/**/*.phtml',
+        '../layout/**/*.xml',
+    ],
+};
